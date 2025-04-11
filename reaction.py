ece6fde0c8795b37c5306e4d80827f815fabf18e
YOLOO-O <785214433@qq.com>
2025-04-11 14:13:53 +0800

    Adding an element of surprise

--- reaction.py
+++ reaction.py
@@ -1,26 +1,30 @@
 # reaction.py
 # Author: Liu Zibo
-# Description: Part 2 - Controlling the light in the Quick Reaction Game using RPi.GPIO
+# ID: 20110005@mail.wit.ie(SETU)/202283890001(NUIST)
+# Date: 2025-04-11
+# Description: Part 2 - Controlling the light + Adding element of surprise in the Quick Reaction Game using RPi.GPIO
 
 import RPi.GPIO as GPIO
 import time
+from random import uniform
 
 GPIO.setmode(GPIO.BCM)
-
 GPIO.setwarnings(False)
-
-
 GPIO.setup(4, GPIO.OUT)
 
 try:
-    print("Quick Reaction Game - Phase: Controlling the light")
-    
+    print("Quick Reaction Game - Phase: Controlling the light + Surprise!")
+
     print("LED ON")
-    GPIO.output(4, GPIO.HIGH) 
-    time.sleep(5) 
+    GPIO.output(4, GPIO.HIGH)
+    
+    delay_time = uniform(5, 10)
+    print(f"Waiting for {delay_time:.2f} seconds...")
+    time.sleep(delay_time)
 
     print("LED OFF")
     GPIO.output(4, GPIO.LOW)
 
 finally:
-    GPIO.cleanup() 
+    GPIO.cleanup()
+
