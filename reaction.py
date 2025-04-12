35b31af3481e8aecc05fb3c155a378306889239e
YOLOO-O <785214433@qq.com>
2025-04-12 10:39:11 +0800

    Get player names

--- reaction.py
+++ reaction.py
@@ -1,21 +1,25 @@
-# reaction.py 
-# Author: Liu Zibo
-# ID: 20110005@mail.wit.ie(SETU)/202283890001(NUIST)
-# Date: 2025-04-11
-# Description: Part 2 - Controlling the light + Detecting button presses using gpiozero (fixed)
-
 from gpiozero import LED, Button
 from time import sleep
 from random import uniform
+
+left_name = input("Enter left player's name: ")
+right_name = input("Enter right player's name: ")
 
 led = LED(4)
 left_button = Button(14)
 right_button = Button(15)
 
-def pressed(button):
-    print(str(button.pin.number) + " won the game")
+game_over = False
 
-print("Quick Reaction Game - Phase: Detecting the buttons")
+def pressed(button):
+    global game_over
+    if button.pin.number == 14:
+        print(left_name + " won the game!")
+    else:
+        print(right_name + " won the game!")
+    game_over = True
+
+print("Quick Reaction Game - Phase: Detecting the buttons + Player names")
 
 try:
     print("LED ON")
@@ -28,12 +32,12 @@ try:
     print("LED OFF")
     led.off()
 
-    print("Waiting for button press... (Press Ctrl+C to quit)")
+    print("Waiting for button press...")
 
     left_button.when_pressed = pressed
     right_button.when_pressed = pressed
 
-    while True:
+    while not game_over:
         sleep(0.1)
 
 except KeyboardInterrupt:
